0dc86b1125a36ab64840c7f82c6fd607ef6aa281
g8up <maduxing@gmail.com>
2019-06-09 14:58:50 +0800

    feat: 采用 npm 包 crx 打包

--- index.js
+++ index.js
@@ -1,47 +1,30 @@
 /**
  * 打包 crx
+ * 依赖：https://www.npmjs.com/package/crx
  */
-const os = require('os');
-const path = require('path');
-const { spawn } = require('child_process');
-const Worker = require('./src/worker');
+
+const fs = require("fs");
+const ChromeExtension = require("crx");
 const util = require('./src/util');
-
-const resolve = dir => (path.resolve(__dirname, dir));
-
-const CLI = resolve('D:\\go-path\\src\\github.com\\mcuadros\\go-crxmake\\cli\\crxmake\\crxmake.exe'); // windows
-const CHROME_MAC = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'; // mac
-
-const run = (...args) => spawn(...args).on('error', (e) => {
-  console.log('args', args);
-  console.log('error:', e);
-});
-
-const isWin32 = os.platform() === 'win32';
 
 module.exports = (SRC_DIR, PEM_DIR, RELEASE_DIR, manifest) => {
   const {
     name,
     version,
   } = manifest;
+  const releaseName = `${util.formatName(name)}-v${version}.crx`;
 
-  // crxmake.exe dist release --key-file=dist.pem
-  const packerWin = () => run(CLI, [
-    `${SRC_DIR}`,
-    `--key-file=${PEM_DIR}`,
-  ]);
-
-  const packerMac = () => run(CHROME_MAC, [
-    `--pack-extension=${SRC_DIR}`,
-    `--pack-extension-key=${PEM_DIR}`,
-  ]);
-
-  const worker = new Worker({
-    srcDir: SRC_DIR,
-    releaseDir: RELEASE_DIR,
-    releaseName: `${util.formatName(name)}-v${version}.crx`,
-    packer: isWin32 ? packerWin : packerMac,
+  const crx = new ChromeExtension({
+    // codebase: "http://localhost:8000/myFirstExtension.crx",
+    privateKey: fs.readFileSync(PEM_DIR),
   });
 
-  worker.run();
-}
+  crx.load(SRC_DIR)
+    .then(crx => crx.pack())
+    .then(crxBuffer => {
+      fs.writeFileSync(`${RELEASE_DIR}/${releaseName}`, crxBuffer);
+    }).catch(err => {
+      console.error(err);
+    });
+};
+
---
+++ test/pack.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+const pack = require('../index')
+const manifest = require('./example/dist/manifest')
+
+const resolver = (dir)=>{
+  return path.resolve(__dirname, dir);
+};
+
+const SRC_DIR = resolver('./example/dist')
+
+const PEM_DIR = resolver('./example/dist.pem');
+
+const RELEASE_DIR = resolver('./release');
+
+pack(SRC_DIR, PEM_DIR, RELEASE_DIR, manifest);
